c64096578025344a1937565726fcf1e0964474e0
VasiliiSokolov <vasilysokolov@mail.ru>
2023-01-14 19:39:17 +0300

    Done!

--- MathematicalVector.cpp
+++ MathematicalVector.cpp
@@ -58,10 +58,10 @@ void vec_add()
 {
     std::cout << "The Vector addition!" << std::endl;
     vec first, second;
-    std::cout << "Enter coordinates of first vector(x y):";
+    std::cout << "Enter coordinates of first vector(x y): ";
     std::cin >> first.x >> first.y;
     
-    std::cout << "Enter coordinates of second vector(x y):";
+    std::cout << "Enter coordinates of second vector(x y): ";
     std::cin >> second.x >> second.y;
     int resultX = first.x + second.x;
     int resultY = first.y + second.y;
@@ -72,10 +72,10 @@ void vec_subtract()
 {
     std::cout << "The Vector subtraction!" << std::endl;
     vec first, second;
-    std::cout << "Enter coordinates of first vector(x y):";
+    std::cout << "Enter coordinates of first vector(x y): ";
     std::cin >> first.x >> first.y;
 
-    std::cout << "Enter coordinates of second vector(x y):";
+    std::cout << "Enter coordinates of second vector(x y): ";
     std::cin >> second.x >> second.y;
     int resultX = first.x - second.x;
     int resultY = first.y - second.y;
@@ -84,13 +84,24 @@ void vec_subtract()
 
 void vec_scale()
 {
+    std::cout << "Vector multiplication by scalar!" << std::endl;
+    vec first;
+    int scalar;
+    std::cout << "Enter coordinates of first vector(x y): ";
+    std::cin >> first.x >> first.y;
+
+    std::cout << "Enter the scalar: ";
+    std::cin >> scalar;
+    int resultX = first.x * scalar;
+    int resultY = first.y * scalar;
+    std::cout << "Result is (" << resultX << "," << resultY << ")." << std::endl;
 }
 
 void vec_length()
 {
     std::cout << "Calculation the length of a vector!" << std::endl;
     vec first;
-    std::cout << "Enter coordinates of vector(x y):";
+    std::cout << "Enter coordinates of vector(x y): ";
     std::cin >> first.x >> first.y;
 
     double length = sqrt(pow(first.x, 2) + pow(first.y, 2));
@@ -100,4 +111,13 @@ void vec_length()
 
 void vec_normalize()
 {
+    std::cout << "Vector normalization!" << std::endl;
+    vec first;
+    std::cout << "Enter coordinates of vector(x y): ";
+    std::cin >> first.x >> first.y;
+
+    double invLength = 1/(sqrt(pow(first.x, 2) + pow(first.y, 2)));
+    double resultX = first.x * invLength;
+    double resultY = first.y * invLength;
+    std::cout << "Result is (" << resultX << "," << resultY << ")." << std::endl;
 }
